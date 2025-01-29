06d58fe1394ab357c710108824d567d107d8c863
egberriospena <egberriospena@gmail.com>
2025-01-29 20:12:13 -0300

    Ending functions addFriend

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* 
+El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+*/
+
+
+// Crea array para guardar los amigos
+let friends = [];
+let friend;
+
+// Función para formatear la lista de amigos
+function formatFriendsList(friends) {
+    if (friends.length === 0) return "";
+    if (friends.length === 1) return friends[0];
+    
+    let lastFriend = friends[friends.length - 1];
+    let remainingFriends = friends.slice(0, -1).join(", ");
+    
+    return `${remainingFriends} y ${lastFriend}`;
+}
+
+// Implementar una función para agregar amigos
+function addTextElement(element, textArray) {
+    let elementHTML = document.querySelector(element);
+    elementHTML.innerHTML = formatFriendsList(textArray);
+}
+
+function cleanInput() {
+    document.querySelector('#amigo').value = '';
+}
+
+function addFriend() {
+    friend = document.getElementById('amigo').value;
+    if (friend == '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        friends.push(friend);
+        console.log(friends);
+        cleanInput();
+        addTextElement('ul', friends);
+        return friend;
+    }
+}
+
